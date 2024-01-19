41626edc0b1057ace8958cf8ee07dc749f58635d
Naveed Riaziat <nriaziat@jhu.edu>
2024-01-19 09:49:30 -0500

    add zigbee implementation

--- jhockey/types.py
+++ jhockey/types.py
@@ -2,6 +2,7 @@ from enum import Enum, auto
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class AruCoTag:
     id: int
@@ -25,16 +26,16 @@ class GameState(Enum):
 
 @dataclass
 class RobotState:
-    x: int = 0  # mm 
-    y: int = 0 # mm  
-    heading: int = 0 # millirad
+    x: int = 0  # mm
+    y: int = 0  # mm
+    heading: int = 0  # millirad
     found: bool = True
 
 
 @dataclass
 class PuckState:
-    x: int  # mm 
-    y: int  # mm 
+    x: int  # mm
+    y: int  # mm
     found: bool
 
 
@@ -43,11 +44,12 @@ class GUIData:
     state: GameState
     seconds_remaining: float
     puck: Optional[PuckState]
-    score: dict[Team : int]
+    score: dict[Team:int]
     score_as_string: str
     robot_states: dict[Team : list[RobotState]]
     aruco_tags: list[AruCoTag]
     cam_connected: bool
+
 
 @dataclass(kw_only=True)
 class BroadcasterMessage:
@@ -55,4 +57,25 @@ class BroadcasterMessage:
     puck: Optional[PuckState]  # optional, if implemented
     # Passing a Team as a key will return a list of RobotStates in order of robot ID
     robots: dict[Team : list[RobotState]]
-    enabled: bool 
+    enabled: bool
+
+    def to_dict(self) -> dict:
+        if self.puck is not None:
+            return {
+                "time": self.time,
+                "puck": self.puck,
+                "robots": self.robots,
+                "enabled": self.enabled,
+            }
+        else:
+            return {
+                "time": self.time,
+                "robots": self.robots,
+                "enabled": self.enabled,
+            }
+
+    def __str__(self) -> str:
+        if self.puck is not None:
+            return f"{self.time},{self.puck.x},{self.puck.y},{self.robots[Team.RED][0].x},{self.robots[Team.RED][0].y},{self.robots[Team.BLUE][0].x},{self.robots[Team.BLUE][0].y},{self.enabled}"
+        else:
+            return f"{self.time},{self.robots[Team.RED][0].x},{self.robots[Team.RED][0].y},{self.robots[Team.BLUE][0].x},{self.robots[Team.BLUE][0].y},{self.enabled}"
