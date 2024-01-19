01d8eee077fd9325fc393e00fd1b668b93adc38e
Anway Pimpalkar <pimpalkaras19.extc@coep.ac.in>
2024-01-19 18:54:46 -0500

    Update main.py

--- micropython/receive_data_1/main.py
+++ micropython/receive_data_1/main.py
@@ -6,7 +6,6 @@ ROBOT_ID = "BA"
 last_payload = None
 
 while True:
-
     payload = xbee.receive()
 
     if payload:
@@ -16,36 +15,36 @@ while True:
 
     # if data:
 
-        # if data.decode() == "?" and last_payload is not None:
+    # if data.decode() == "?" and last_payload is not None:
 
     if last_payload is not None:
-
-        receivedMsg = last_payload['payload'].decode('utf-8')
-
+        receivedMsg = last_payload["payload"].decode("utf-8")
 
         if receivedMsg:
-            
-            # stdout.buffer.write(str(receivedMsg + '\n').encode())
-
-            startString = receivedMsg.find("{")
+            startString = receivedMsg.find("{") + 1
             endString = receivedMsg.find("}", startString)
 
             string = receivedMsg[startString:endString]
-            
+
             if endString == -1:
-                receivedMsg2 = xbee.receive()['payload'].decode('utf-8')
-                endString2 = receivedMsg.find("}")
+                receivedMsg2 = xbee.receive()["payload"].decode("utf-8")
+                endString2 = receivedMsg2.find("}")
 
                 if endString2 < receivedMsg2.find("{"):
-                    string = string + receivedMsg2[:(endString2 + 1)]
-
-            string = receivedMsg[startString + 1:endString]
+                    string = string + receivedMsg2[:(endString2)]
 
             string = string.replace("'", "")
             string = string.split(", ")
             string = [x.split(": ") for x in string]
             data = {x[0]: x[1] for x in string}
 
-            out = str(data["match"]) + "," + str(data["time"]) + "," + str(data[ROBOT_ID]) + "\n"
+            out = (
+                str(data["match"])
+                + ","
+                + str(data["time"])
+                + ","
+                + str(data[ROBOT_ID])
+                + "\n"
+            )
 
             stdout.buffer.write(out.encode())
