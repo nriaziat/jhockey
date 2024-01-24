631f797af7755479273a93ddf7fd999245cdcd8d
Anway Pimpalkar <pimpalkaras19.extc@coep.ac.in>
2024-01-23 23:26:34 -0500

    ..

--- micropython/receive_data_1/parse_string.py
+++ micropython/receive_data_1/parse_string.py
@@ -1,16 +1,25 @@
-def parse_string(data):
-    startLen = 1
-    timeLen = 4
-    robotIDLen = 2
-    coordLen = 3
-    angleLen = 3
+""""
+Function to parse the string of data received from the robot.
+"""
 
+
+def parse_string(data, parsingParameters):
+    # Get the parsing parameters from the list
+    startLen = parsingParameters[0]
+    timeLen = parsingParameters[1]
+    robotIDLen = parsingParameters[2]
+    coordLen = parsingParameters[3]
+    angleLen = parsingParameters[4]
+
+    # Create a dictionary to store the parsed data
     parsedData = {}
 
+    # Parse the data
     parsedData["start"] = data[0:startLen]
     parsedData["time"] = data[startLen : startLen + timeLen]
     parsedData["matchbit"] = data[startLen + timeLen]
 
+    # Get the robot data
     i = startLen + timeLen + 1
 
     while i < len(data):
